98462c3d0d8ff907e45a5cdc0c781148661d954b
Chengyu Liou <chengyu.liou@onedegree.hk>
2025-04-14 16:14:18 +0800

    fix: enhance error handling in evaluate function and add charset-normalizer dependency

--- cli/commands/evaluate/eval.py
+++ cli/commands/evaluate/eval.py
@@ -3,9 +3,9 @@ def evaluate(csv_path: str, source_code_path: str, n8n_workflow_webhook_url: str
     import pandas as pd
     import requests
     import sklearn.metrics
+    from charset_normalizer import from_path
     from pandas import read_csv
     from tabulate import tabulate
-    from tenacity import RetryError, Retrying, stop_after_attempt
     from tqdm import tqdm
 
     dataset = read_csv(csv_path)
@@ -22,51 +22,54 @@ def evaluate(csv_path: str, source_code_path: str, n8n_workflow_webhook_url: str
         colour="blue",
         bar_format="{desc}: {percentage:3.0f}%|{bar}| {n_fmt}/{total_fmt} files [Time: {elapsed}]",
     ):
+        try:
+            if pd.isna(row["file_name"]) or pd.isna(row["answer"]):
+                print("Invalid row, stop...")
+                break
 
-        if pd.isna(row["file_name"]) or pd.isna(row["answer"]):
-            print("Invalid row, stop...")
-            break
-
-        with open(source_code_path + row["file_name"]) as f:
-            file = f.read()
-            data = {"prompt": file}
-            try:
-                for attempt in Retrying(stop=stop_after_attempt(3)):
-                    with attempt:
-                        response = requests.post(n8n_workflow_webhook_url, json=data)
-                        if response.status_code == 200:
-                            break
-                        else:
-                            tqdm.write(
-                                "\033[91m❌ n8n Workflow response abnormal, retry...: {}\033[0m".format(
-                                    response.text
-                                )
-                            )
-                            raise Exception("n8n Workflow response abnormal")
-            except RetryError:
-                print("Failed to retry the request")
-                tqdm.write(
-                    "\033[91m❌ Error processing {}\033[0m".format(row["file_name"])
-                )
-                continue
-
-            try:
-                json_data = response.json()
-
-                y_pred.append(1 if "output" in json_data and json_data["output"] else 0)
-                y_true.append(int(row["answer"]))
-                tqdm.write(
-                    "\033[92m✅ Successfully processed: {}\033[0m".format(
-                        row["file_name"]
+            with open(
+                source_code_path + row["file_name"],
+                encoding=from_path(source_code_path + row["file_name"]).best().encoding,
+            ) as f:
+                file = f.read()
+                data = {"prompt": file}
+                response = requests.post(n8n_workflow_webhook_url, json=data)
+                if response.status_code != 200:
+                    tqdm.write(
+                        "\033[91m❌ n8n Workflow response abnormal, retry...: {}\033[0m".format(
+                            response.text
+                        )
                     )
-                )
-            except Exception as e:
-                tqdm.write(
-                    "\033[91m❌ Error processing {}: {}\033[0m".format(
-                        row["file_name"], str(e)
+                    continue
+
+                try:
+                    json_data = response.json()
+                    if any(
+                        "output" in obj and obj["output"] != [] for obj in json_data
+                    ):
+                        y_pred.append(1)
+                    else:
+                        y_pred.append(0)
+                    y_true.append(int(row["answer"]))
+                    tqdm.write(
+                        "\033[92m✅ Successfully processed: {}\033[0m".format(
+                            row["file_name"]
+                        )
                     )
+                except Exception as e:
+                    tqdm.write(
+                        "\033[91m❌ Error processing {}: {}\033[0m".format(
+                            row["file_name"], str(e)
+                        )
+                    )
+                    continue
+        except:
+            tqdm.write(
+                "\033[91m❌ Error processing {}: {}\033[0m".format(
+                    row["file_name"], str(e)
                 )
-                continue
+            )
+            continue
 
     tn, fp, fn, tp = sklearn.metrics.confusion_matrix(
         y_pred=y_pred, y_true=y_true, labels=[0, 1]
